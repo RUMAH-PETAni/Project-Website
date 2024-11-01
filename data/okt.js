1f35d38be8fd96261a0e3abc473a3d0136411674
RUMAH-PETAni <168377278+RUMAH-PETAni@users.noreply.github.com>
2024-11-01 11:42:44 +0700

    Add files via upload

---
+++ data/okt.js
@@ -0,0 +1,13 @@
+var okt_json = {
+"type": "FeatureCollection",
+"name": "okt",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gridcode": 10, "luas": 0.0735746987 }, "geometry": { "type": "Point", "coordinates": [ 103.6131, -4.91135 ] } },
+{ "type": "Feature", "properties": { "gridcode": 10, "luas": 0.02452498625 }, "geometry": { "type": "Point", "coordinates": [ 103.6134, -4.90875 ] } },
+{ "type": "Feature", "properties": { "gridcode": 10, "luas": 0.0122624921 }, "geometry": { "type": "Point", "coordinates": [ 103.61315, -4.90895 ] } },
+{ "type": "Feature", "properties": { "gridcode": 10, "luas": 0.26977453183 }, "geometry": { "type": "Point", "coordinates": [ 103.613431818, -4.90954090909 ] } },
+{ "type": "Feature", "properties": { "gridcode": 10, "luas": 0.04904988825 }, "geometry": { "type": "Point", "coordinates": [ 103.6131, -4.9101 ] } },
+{ "type": "Feature", "properties": { "gridcode": 10, "luas": 0.02451516307 }, "geometry": { "type": "Point", "coordinates": [ 104.3527, -4.86945 ] } }
+]
+}
